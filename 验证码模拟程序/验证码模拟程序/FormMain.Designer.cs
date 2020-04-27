4f8e230b9ddcd9b0308281ed1ed962c95066a171
张磊 <codelei@163.com>
2020-04-27 11:49:10 +0800

    初始完成的项目

---
+++ 验证码模拟程序/验证码模拟程序/FormMain.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace 验证码模拟程序
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelRead = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(619, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击完成验证";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelRead
+            // 
+            this.labelRead.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRead.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelRead.Location = new System.Drawing.Point(12, 147);
+            this.labelRead.Name = "labelRead";
+            this.labelRead.Size = new System.Drawing.Size(601, 75);
+            this.labelRead.TabIndex = 1;
+            this.labelRead.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.Orange;
+            this.textBox1.Location = new System.Drawing.Point(208, 412);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 46);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTitle.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labelTitle.Location = new System.Drawing.Point(12, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(770, 75);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "验证码";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(5, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "请输入验证码:";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTimer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.labelTimer.Location = new System.Drawing.Point(12, 247);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(770, 75);
+            this.labelTimer.TabIndex = 5;
+            this.labelTimer.Text = "60 秒";
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelTimer.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(619, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 75);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "获取验证码";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 470);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelRead);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "验证码模拟程序 ---- v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelRead;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 验证码模拟程序/验证码模拟程序/FormMain.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 验证码模拟程序
+{
+    public partial class FormMain : Form
+    {
+        //倒计时时间
+        int i = 60;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //判断输入验证
+            if (textBox1.Text == "" || textBox1.TextLength != 6)
+            {
+                MessageBox.Show("验证码错误，请检查错误原因~~\n可能有以下原因：\n1.验证码为空\n2.验证码位数不正确\n3.验证码输入不正确","验证失败",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                //输入文本控件值为空
+                textBox1.Text = null;
+                //随机显示验证码
+                Random rd = new Random();
+                //验证码存储变量
+                int yzm;
+                //判断随机验证码长度是否等于6位
+                for (;;)
+                {
+                    yzm = rd.Next(000000, 999999);
+                    if (yzm.ToString().Length == 6)
+                    {
+                        break;
+                    }
+                }
+                //刷新显示文本
+                labelRead.Text = yzm.ToString()+" 秒";
+            }
+            //判断输入正确时提示
+            else if (textBox1.Text.Equals(labelRead.Text))
+            {
+                MessageBox.Show("验证码输入正确！！！","验证成功", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                //输入文本控件值为空
+                textBox1.Text = null;
+                //随机显示验证码
+                Random rd = new Random();
+                //验证码存储变量
+                int yzm;
+                //判断随机验证码长度是否等于6位
+                for (;;)
+                {
+                    yzm = rd.Next(000000, 999999);
+                    if (yzm.ToString().Length == 6)
+                    {
+                        break;
+                    }
+                }
+                //刷新显示文本
+                labelRead.Text = yzm.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //随机显示验证码
+            Random rd = new Random();
+            //验证码存储变量
+            int yzm;
+            //判断随机验证码长度是否等于6位
+            for (;;)
+            {
+                yzm = rd.Next(000000, 999999);
+                if (yzm.ToString().Length == 6)
+                {
+                    break;
+                }
+            }
+            //刷新显示文本
+            labelRead.Text = yzm.ToString();
+            //展示验证码倒计时控件
+            labelTimer.Visible = true;
+            //启动验证码倒计时
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i == 0)
+            {
+                labelTimer.Text = "验证码已过期,请重新获取。";
+                //关闭倒计时控件
+                timer1.Enabled = false;
+            }
+            else
+            {
+                //每秒减一
+                i--;
+                //刷新文本
+                labelTimer.Text = i.ToString();
+            }
+
+        }
+    }
+}
